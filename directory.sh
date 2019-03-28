a60de80113e371550c74735be5fdcc5f2f4a770c
JOMOL <jomoljosemon2016@gmail.com>
2019-03-28 14:36:54 +0530

    initial commit

---
+++ directory.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+echo "The current working directory: $PWD"
+echo "The current user :$USER"
+echo "The current shell : $SHELL"
+echo "The current path: $PATH"
+echo "The current home directory : $HOME"
+echo "The current System Architecture : $(arch)"
